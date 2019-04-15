67ce5f35a5dcb27b8b892d32c5ddb2fb86936054
YixiangWebsterWang <yixiang.wang@yale.edu>
2019-04-15 11:25:29 -0400

    Update diffmap.m

--- diffmap.m
+++ diffmap.m
@@ -11,6 +11,7 @@ function [Dmap, vals] = diffmap(xs, t, m, sigma)
 %       Dmap     Diffusion map (embedded coordinates)
 %       vals     Corresponding eigenvalues
 
+    %Calcultae pairwise distances
     pdist_xs = pdist(xs);
     
     if isempty(sigma)
@@ -18,17 +19,23 @@ function [Dmap, vals] = diffmap(xs, t, m, sigma)
     end
     disp(['Sigma is: ', num2str(sigma)])
     
+    %Calculate weight matrix
     distMatrix = squareform(pdist_xs);
     W = exp(-distMatrix./(2*sigma^2));
     
+    %Calculate weighted M matrix
     d = sum(W,1);
     M = W./d';
     k = m+1;
     
+    %Get the first k eigenvectors/eigenvalues
     [psi, L] = eigs(M, k);
     
+    %Omit the trivial first dimension
     vals = diag(L);
     vals = vals(2:end);
     Dmap = psi(:,2:end).*vals'.^t;
+    
+    disp('Diffusion map analysis is done!')
     
 end
